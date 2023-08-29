85081a91dd12e6c2b9eabc314e8e2b5430cbcda2
Neolux Lee <neolux.lee@foxmail.com>
2023-08-29 10:10:31 +0800

    Changes

---
+++ 1-RW/ReadWriteImage.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+import sys
+
+img = cv.imread(cv.samples.findFile("icon.jpg"))
+
+if img is None:
+    sys.exit("Could not read the image.")
+
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
+
+# cv.destroyAllWindows()
+cv.destroyWindow("Display window")
+
+# print(ord("s"))
