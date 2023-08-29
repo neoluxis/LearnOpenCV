50398fca98090abc9ca8f3402c0ce6a5c1297386
Neolux Lee <neolux.lee@foxmail.com>
2023-08-29 10:28:28 +0800

    vscode run. Fix CWD

--- 1-RW/ReadWriteImage.py
+++ 1-RW/ReadWriteImage.py
@@ -11,7 +11,7 @@ cv.imshow("Display window", img)
 k = cv.waitKey(0)
 
 if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+    cv.imwrite("saved.png", img)
 
 # cv.destroyAllWindows()
 cv.destroyWindow("Display window")
